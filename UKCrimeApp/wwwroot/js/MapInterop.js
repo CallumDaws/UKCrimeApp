b19330b5ce74e8c6b1b31b053238fb33cc028050
DESKTOP-UB1KVE3\Callum Daws <callumdaws@hotmail.co.uk>
2024-02-27 23:22:41 +0000

    fix map button

--- UKCrimeApp/wwwroot/js/MapInterop.js
+++ UKCrimeApp/wwwroot/js/MapInterop.js
@@ -1,7 +1,13 @@
+let map; // Holds the map instance
+
 function loadMap() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data © OpenStreetMap contributors',
-        maxZoom: 18,
-    }).addTo(map);
+    if (!map) { // Check if the map has not been initialized
+        map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data © OpenStreetMap contributors',
+            maxZoom: 18,
+        }).addTo(map);
+    } else {
+        map.invalidateSize(); // Adjust the map size if the map container size changed
+    }
 }
